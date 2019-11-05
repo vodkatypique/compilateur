031c4354697ab7cc30d887e5602dda8b340653d9
Vodkatypique <clement.caffin@gmail.com>
2019-11-05 16:06:33 +0100

    analyseLexical

--- compilo.py
+++ compilo.py
@@ -1,4 +1,7 @@
-class Memoire():
+import re
+
+
+class Programme():
     def __init__(self):
         self.pile = list()
         self.pointeur_pile = 0
@@ -121,8 +124,205 @@ class Memoire():
             getattr(self, self.INST[0])(*argument)
 
 
-prog = Memoire()
-prog.pcode.extend([
+class AnalyseurLexical():
+    """ brouillon:
+        self.CONSTS = "^((const " + "(" + self.ID[1:-1] + " = [0-9]+;)+)|())$"
+        self.VARS = "^((var " + self.ID[1:-1] + "(, " + self.ID[1:-1] + ")*;)|())$"
+        self.LIRE = "^read (" + self.ID[1:-1] + "(, " + self.ID[1:-1] + ")*\)$"
+        self.RELOP = "^(=|<>|<|>|<=|>=)$"
+        self.ADDOP = "^(+|-)$"
+        self.MULOP = "^(*|/)$"
+        
+        self.EXPR = "^" + self.TERM[1:-1] + "( " + self.ADDOP[1:-1] + " " + self.TERM[1:-1] + ")*$"
+
+        self.FACT = "^(" + self.ID[1:-1] + "|[0-9]+|" + "\(" + self.EXPR + "\))$"
+
+        self.TERM = "^" + self.FACT[1:-1] + "( " + self.MULOP[1:-1] + self.FACT[1:-1] + ")*$"
+
+        self.COND = "^" + self.EXPR[1:-1] + " " + self.RELOP[1:-1] + " " + self.EXPR[1:-1] + "$"
+        self.ECRIRE = "^write \(" + self.EXPR[1:-1] + "(, " + self.EXPR[1:-1] + ")*\)$"
+        self.SI = "^if " + self.COND[1:-1] + " then " + self.INST[1:-1] + "$"
+        self.TANTQUE = "^while " + self.COND[1:-1] + " do " + self.INST[1:-1] + "$"    
+        self.AFFEC = "^" + self.ID[1:-1] + " := " + self.EXPR[1:-1] + "$"
+        
+        self.INST = "^(" + self.INSTS[1:-1] + "|" + self.AFFEC[1:-1] + "|" + self.SI[1:-1] + "|" + self.TANTQUE[1:-1] + "|" + self.ECRIRE[1:-1] + "|" + self.LIRE[1:-1] + "|())$"
+        self.INSTS = "^begin " + self.INST[1:-1] + "(;" + self.INST[1:-1]+")* end$"
+        
+        self.BLOCK = "^" + self.CONSTS[1:-1] + " " + self.VARS[1:-1] + self.INSTS[1:-1] + "$"
+        self.PROGRAM = "^program " + self.ID[1:-1] + ";" + self.BLOCK[1:-1] + "."
+    """
+    def __init__(self):
+        self.tokens = {
+            'id_token': "^(?!^program$|^const$|^var$|^begin$|^end$|^if$|^then$|^while$|^do$|^write$|^read$)[a-zA-Z]+[0-9]*$",
+            'num_token': "^[1-9]+[0-9]*$",
+            'plus_token': "^\+$",
+            'moins_token': "^-$",
+            'mul_token': "^\*$",
+            'div_token': "^/$",
+            'egal_token': "^=$",
+            'diff_token': "^!=$",
+            'inf_token': "^<$",
+            'sup_token': "^>$",
+            'inf_egal_token': "^<=$",
+            'sup_egal_token': "^>=$",
+            'par_ouv_token': "^\($",
+            'par_fer_token': "^\)$",
+            'virg_token': "^,$",
+            'pt_virg_token': "^;$",
+            'pt_token': "^.$",
+            'affec_token': "^:=$",
+            'begin_token': "^begin$",
+            'end_token': "^end$",
+            'if_token': "^if$",
+            'while_token': "^while$",
+            'then_token': "^then$",
+            'do_token': "^do$",
+            'write_token': "^write$",
+            'read_token': "^read$",
+            'const_token': "^const$",
+            'var_token': "^var$",
+            'program_token': "^program$",
+            # token inconnu ?
+        }
+        self.index_token = 0
+        self.sym = None
+        self.val = None
+        self.tokens_code = None
+
+    def tokenizeCode(self, code):
+        self.tokens_code = code.split()
+
+    def test(self, t):
+        print(t)
+        print(self.index_token)
+        print(self.tokens_code)
+        print("\n")
+
+        if not re.match(t, self.tokens_code[self.index_token]):
+            print('Erreur : analyseurLexical')
+        else:
+            self.index_token += 1
+
+    def program(self):
+        self.test(self.tokens['program_token'])
+        self.test(self.tokens['id_token'])
+        self.test(self.tokens['pt_virg_token'])
+        self.block()
+        if not re.match(self.tokens['pt_virg_token'], self.tokens_code[self.index_token]): #erreur sujet
+            print(self.tokens_code[self.index_token])
+            print(self.tokens['pt_virg_token'])
+            print('Erreur : program')
+
+    def block(self):
+        if re.match(self.tokens['const_token'], self.tokens_code[self.index_token]):
+            self.consts()
+        if re.match(self.tokens['var_token'], self.tokens_code[self.index_token]):
+            self.vars()
+        self.insts()
+
+    def consts(self):
+        self.test(self.tokens['id_token'])
+        while not re.match(self.tokens['id_token'], self.tokens_code[self.index_token]):
+            self.test(self.tokens['id_token'])  # repetition du test du while, inutile je pense
+            self.test(self.tokens['egal_token'])
+            self.test(self.tokens['num_token'])
+            self.test(self.tokens['pt_virg_token'])
+
+    def vars(self):
+        self.test(self.tokens['var_token'])
+        self.test(self.tokens['id_token'])
+        while re.match(self.tokens['virg_token'], self.tokens_code[self.index_token]):
+            self.index_token += 1
+            self.test(self.tokens['id_token'])
+        self.test(self.tokens['pt_virg_token'])
+
+    def insts(self):
+        self.test(self.tokens['begin_token'])
+        self.inst()
+        while re.match(self.tokens['pt_virg_token'], self.tokens_code[self.index_token]):
+            self.index_token += 1
+            self.inst()
+        self.test(self.tokens['end_token'])
+
+    def inst(self):
+        if re.match(self.tokens['id_token'], self.tokens_code[self.index_token]):
+            self.affec()
+        elif re.match(self.tokens['if_token'], self.tokens_code[self.index_token]):
+            self.si()
+        elif re.match(self.tokens['while_token'], self.tokens_code[self.index_token]):
+            self.tantque()
+        elif re.match(self.tokens['begin_token'], self.tokens_code[self.index_token]):
+            self.insts()
+        elif re.match(self.tokens['write_token'], self.tokens_code[self.index_token]):
+            self.ecrire()
+        elif re.match(self.tokens['read_token'], self.tokens_code[self.index_token]):
+            self.read()
+
+    def affec(self):
+        self.test(self.tokens['id_token'])
+        self.test(self.tokens['affec_token'])
+        self.expr()
+    
+    def si(self):
+        self.test(self.tokens['if_token'])
+        self.cond()
+        self.test(self.tokens['then_token'])
+        self.inst()
+    
+    def tantque(self):
+        self.test(self.tokens['while_token'])
+        self.cond()
+        self.test(self.tokens['do_token'])
+        self.inst()
+    
+    def ecrire(self):
+        self.test(self.tokens['write_token'])
+        self.test(self.tokens['par_ouv_token'])
+        self.expr()
+        while re.match(self.tokens['virg_token'], self.tokens_code[self.index_token]):
+            self.index_token += 1
+            self.expr()
+        self.test(self.tokens['par_fer_token'])
+
+    def lire(self):
+        self.test(self.tokens['read_token'])
+        self.test(self.tokens['par_ouv_token'])
+        self.test(self.tokens['id_token'])
+        while re.match(self.tokens['virg_token'], self.tokens_code[self.index_token]):
+            self.index_token += 1
+            self.test(self.tokens['id_token'])
+        self.test(self.tokens['par_fer_token'])
+
+    def expr(self):
+        self.term()
+        while re.match(self.tokens['plus_token'], self.tokens_code[self.index_token]) or re.match(self.tokens['moins_token'], self.tokens_code[self.index_token]):
+            self.index_token += 1
+            self.term()
+
+    def cond(self):
+        self.expr()
+        if re.match(self.tokens['egal_token'], self.tokens_code[self.index_token]) or re.match(self.tokens['diff_token'], self.tokens_code[self.index_token]) or re.match(self.tokens['inf_token'], self.tokens_code[self.index_token]) or re.match(self.tokens['sup_token'], self.tokens_code[self.index_token]) or re.match(self.tokens['inf_egal_token'], self.tokens_code[self.index_token]) or re.match(self.tokens['sup_egal_token'], self.tokens_code[self.index_token]):
+            self.index_token += 1
+            self.expr()
+    
+    def term(self):
+        self.fact()
+        while re.match(self.tokens['mult_token'], self.tokens_code[self.index_token]) or re.match(self.tokens['div_token'], self.tokens_code[self.index_token]):
+            self.index_token += 1
+            self.fact()
+    
+    def fact(self):
+        if re.match(self.tokens['id_token'], self.tokens_code[self.index_token]) or re.match(self.tokens['num_token'], self.tokens_code[self.index_token]):
+            self.index_token += 1
+        else:
+            self.test(self.tokens['par_ouv_token'])
+            self.expr()
+            self.test(self.tokens['par_fer'])
+
+
+"""
+#prog = Programme()
+#prog.pcode.extend([
     "INT 2",
     "LDA 0",
     "INN",
@@ -144,4 +344,10 @@ prog.pcode.extend([
     "HLT"])
 
 #print(prog.pcode)
-prog.convert_parenthese_and_run()
+#prog.convert_parenthese_and_run()
+"""
+
+source = "program P1 ; var VAR1 ; \n begin end ;"
+analyseur = AnalyseurLexical()
+analyseur.tokenizeCode(source)
+analyseur.program()
