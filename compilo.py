826126b2b82feb74bcd1b80e55f62cf994f7f4fd
Vodkatypique <clement.caffin@gmail.com>
2019-12-31 17:11:35 +0100

    fonctionne :)

--- compilo.py
+++ compilo.py
@@ -1,10 +1,7 @@
 import Programme
 import Programme.Analyseur
 
-
 """
-prog = Programme()
-prog.pcode.extend([
     "INT 2",
     "LDA 0",
     "INN",
@@ -25,14 +22,12 @@ prog.pcode.extend([
     "PRN",
     "HLT"])
 
-print(prog.pcode)
-prog.convert_parenthese_and_run()
-
 """
+
 
 programme_init = Programme.Programme()
 #source = "program P1; var A, B; \n begin read ( A ); read ( B ); write ( A ); write ( B ) end."
-source = "program P1; var A, B; begin read ( A ); while A != 0 do begin read ( A ); B := A + B; end; write ( B ) end."
+source = "program P1; var A, B; begin read ( A ); B := A; while A != 0 do begin read ( A ); B := A + B; end; write ( B ) end."
 analyseur = Programme.Analyseur.AnalyseurLexical(programme_init)
 analyseur.tokenizeCode(source)
 analyseur.program()
