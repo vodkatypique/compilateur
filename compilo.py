6c4444518f77c87c51b281519f9570be446ec5e9
Vodkatypique <clement.caffin@gmail.com>
2019-12-31 18:07:51 +0100

    vodkatypique

--- compilo.py
+++ compilo.py
@@ -2,29 +2,6 @@ import Programme
 import Programme.Analyseur
 import parseTxt
 
-"""
-    "INT 2",
-    "LDA 0",
-    "INN",
-    "LDA 1",
-    "LDA 0",
-    "LDV",
-    "LDA 1",
-    "LDV",
-    "ADD",
-    "STO",
-    "LDA 0",
-    "LDV",
-    "LDI 0",
-    "EQL",
-    "BZE 1",
-    "LDA 1",
-    "LDV",
-    "PRN",
-    "HLT"])
-
-"""
-
 if __name__ == "__main__": 
     programme_init = Programme.Programme()
     source = parseTxt.parse()
