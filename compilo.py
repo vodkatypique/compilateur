670c983130d855036f04ba1aafdc2c0830c8d220
Vodkatypique <clement.caffin@gmail.com>
2020-01-01 19:18:05 +0100

    fin

--- compilo.py
+++ compilo.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     programme_init = Programme.Programme()
     source = parseTxt.parse()
     #print(source)
+    #pour une source a la main, ecire :
+    #source = "votre code source" (s'inspirer de la ligne 8 en cas de doute)
     analyseur = Programme.Analyseur.AnalyseurLexical(programme_init)
     analyseur.tokenizeCode(source)
     analyseur.program()
